9c77efe09476a4174dc40fca5fe7ba742946e72d
Yazzy Boo <yazzyboo@Chazzs-MacBook-Pro.local>
2024-01-11 18:14:30 -0500

    only one example is done

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -6,10 +6,10 @@
 
 let launchReady = false;
 let fuelLevel = 17000;
-// let crewStatus = true;
-// let computerStatus = 'green';
+let crewStatus = true;
+let computerStatus = 'green';
 
-if (fuelLevel >= 20000) {
+if (fuelLevel >= 20000){
    console.log('Fuel level cleared.');
    launchReady = true;
 } else {
